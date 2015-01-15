92d387f0b862e9d6895306df92a09071503ecabc
zsyu <jsyu17@gmail.com>
2015-01-15 23:27:26 +0800

    First Commit
    Source Created

---
+++ Rename/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Rename
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+		/// 修改這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.linkBlog = new System.Windows.Forms.LinkLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbSearch = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(10, 26);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(363, 22);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "C:\\Users\\Administrator\\Downloads\\Renamesourcecode\\PCMD";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(293, 50);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(79, 18);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Enabled = false;
+            this.txtSearch.Location = new System.Drawing.Point(10, 86);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(363, 22);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Prefix :";
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(10, 126);
+            this.txtReplace.Margin = new System.Windows.Forms.Padding(2);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(363, 22);
+            this.txtReplace.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(10, 151);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(362, 42);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start Rename";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // linkBlog
+            // 
+            this.linkBlog.AutoSize = true;
+            this.linkBlog.Location = new System.Drawing.Point(110, 204);
+            this.linkBlog.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkBlog.Name = "linkBlog";
+            this.linkBlog.Size = new System.Drawing.Size(145, 12);
+            this.linkBlog.TabIndex = 8;
+            this.linkBlog.TabStop = true;
+            this.linkBlog.Text = "focus1921\'s web development";
+            this.linkBlog.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkBlog_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Original source by";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(226, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Modified by Kevin YU [ jsyu17(at)gmail.com ]";
+            // 
+            // cbSearch
+            // 
+            this.cbSearch.AutoSize = true;
+            this.cbSearch.Location = new System.Drawing.Point(10, 65);
+            this.cbSearch.Name = "cbSearch";
+            this.cbSearch.Size = new System.Drawing.Size(55, 16);
+            this.cbSearch.TabIndex = 11;
+            this.cbSearch.Text = "Search";
+            this.cbSearch.UseVisualStyleBackColor = true;
+            this.cbSearch.CheckedChanged += new System.EventHandler(this.cbSearch_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 337);
+            this.Controls.Add(this.cbSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkBlog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Batch File Rename for .NET ASPX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.TextBox txtSearch;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtReplace;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.LinkLabel linkBlog;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox cbSearch;
+	}
+}
+
---
+++ Rename/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Rename
+{
+    public partial class Form1 : Form
+    {
+        private string strSearch;
+        private string strReplace;
+        private int count;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region CheckIsTextFile
+        /// <summary>
+        /// Checks the file is textfile or not.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns></returns>
+        public static bool CheckIsTextFile(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            bool isTextFile = true;
+            try
+            {
+                int i = 0;
+                int length = (int)fs.Length;
+                byte data;
+                while (i < length && isTextFile)
+                {
+                    data = (byte)fs.ReadByte();
+                    isTextFile = (data != 0);
+                    i++;
+                }
+                return isTextFile;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// StreamWriter寫入方法
+        /// </summary>
+        private void StreamWriterMetod(string sPath, string sContent, string sFileName)
+        {
+            try
+            {
+                FileStream fsFile = new FileStream(sPath + "\\" + sFileName, FileMode.Create);
+                StreamWriter swWriter = new StreamWriter(fsFile, Encoding.Default);
+                //寫入數據
+                swWriter.Write(sContent);
+                swWriter.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        /// <summary>
+        /// StreamWriter讀取方法
+        /// </summary>
+        private string StreamReaderMetod(string sPath)
+        {
+            string sLines = "";
+            try
+            {
+                FileStream fsFile = new FileStream(sPath, FileMode.Open);
+                StreamReader srReader = new StreamReader(fsFile, Encoding.Default);
+                //讀取文件(讀取大文件時，最好不要用此方法)
+                sLines = srReader.ReadToEnd();
+
+                srReader.Close();
+                return sLines;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        private void DoRename(string path, Hashtable ht, ArrayList myList)
+        {
+            //遞迴計數器累加1
+            count++;
+
+            //new一個目標目錄的DirectoryInfo
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            //防呆，確認目標目錄確實存在
+            if (!dirInfo.Exists)
+            {
+                MessageBox.Show("Diretory not exist!");
+                return;
+            }
+
+            //dirInfo.GetFiles()取回目錄下檔案清單(FileInfo陣列)
+            FileInfo[] files = dirInfo.GetFiles();
+
+
+            string keyName = "";
+            foreach (FileInfo fileInfo in files)
+            {
+                //foreach檢查每一個FileInfo檔名，搜尋並取代檔名。
+                string oldFileName = fileInfo.FullName;
+
+
+                //string strExtnName=Path.GetExtension(oldFileName);//取得副檔名
+                bool isTextFile = CheckIsTextFile(oldFileName);//是否為文字檔
+                if (isTextFile)
+                {
+                    if (!fileInfo.Name.StartsWith(strReplace))
+                    {
+                        ht.Add(fileInfo.Name, fileInfo.FullName);//把所有檔名存進集合(鍵:檔名;值:檔路徑)
+                        if (fileInfo.Name.Contains(".aspx"))
+                        {
+
+                            keyName = fileInfo.Name.Replace(".aspx", "");
+                            keyName = keyName.Replace(".cs", "");
+                            if (!myList.Contains(keyName))
+                            {
+                                myList.Add(keyName);
+                            }
+
+                        }
+                    }
+
+                }
+                else
+                {
+
+                    // MessageBox.Show(fileInfo.Name + " is NOT txt file!");
+                }
+
+            }
+
+            //dirInfo.GetDirectories()取回目錄下子目錄清單(DirectoryInfo陣列)
+            DirectoryInfo[] subDirs = dirInfo.GetDirectories();
+            foreach (DirectoryInfo subDirInfo in subDirs)
+            {
+                //遞迴呼叫DoRename()方法，傳入子目錄。
+                DoRename(subDirInfo.FullName, ht, myList);
+            }
+
+            //完成本次遞迴，計數器累減1
+            count--;
+
+            if (count == 0)
+            {
+
+                //重新命名與檔案內容置換
+                ReplaceAndWriteFile(ht, myList);
+            }
+
+        }
+
+        /// <summary>
+        /// 重新命名與檔案內容置換
+        /// </summary>
+        /// <param name="ht"></param>
+        private void ReplaceAndWriteFile(Hashtable ht, ArrayList al)
+        {
+            btnStart.Text = "Renaming...";
+
+            foreach (DictionaryEntry de in ht)
+            {
+                //計數器累加1
+                count++;
+                string sOrgLines = StreamReaderMetod(de.Value.ToString());//讀取檔案內容
+                string tmpLines = sOrgLines;
+
+
+                string tmpKeyWithExtn = "";
+                tmpKeyWithExtn = de.Key.ToString();
+
+                //////將內容特定字串取代
+                for (int i = 0; i < al.Count; i++)
+                {
+                    //Regex.Match whole words
+                    if (Regex.Match(tmpLines, @"\b" + al[i].ToString() + @"\b", RegexOptions.Singleline | RegexOptions.IgnoreCase).Success)
+                    {
+                        tmpLines = tmpLines.Replace(al[i].ToString(), strReplace + al[i].ToString());
+                    }
+
+                }
+
+
+                //if (de.Value.ToString().IndexOf(de.Key.ToString()) != -1)
+                //{
+                //    //newFileName = de.Value.ToString().Replace(de.Key.ToString(), strReplace + de.Key.ToString());
+                //    newFileName = strReplace + de.Key.ToString();
+                //    //檔案更名
+                //    //fileInfo.MoveTo(newFileName);
+                //}
+
+
+                // 檢查資料夾是否存在，不存在則新增 
+                string strDirPath = txtPath.Text + "\\Output" + DateTime.Now.ToString("yyMMddhh") + "";
+
+
+                if (!Directory.Exists(strDirPath))
+                {
+                    Directory.CreateDirectory(strDirPath);
+                }
+                //寫入新的內容到特定資料夾
+                StreamWriterMetod(strDirPath, tmpLines, strReplace + tmpKeyWithExtn);
+
+                //計數器累減1
+                count--;
+            }
+            if (count == 0)
+            {
+                MessageBox.Show("Rename complete!");
+                btnStart.Text = "Start Rename";
+                btnStart.Enabled = true;
+            }
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //檢查txtPath，不可為空
+            if (string.IsNullOrEmpty(txtPath.Text))
+            {
+                MessageBox.Show("Enter or select target directory!");
+                return;
+            }
+           
+            //檢查txtSearch，不可為空
+            if (string.IsNullOrEmpty(txtSearch.Text)&&cbSearch.Checked==true)
+            {
+                MessageBox.Show("Enter text you want to search");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtReplace.Text))
+            {
+                MessageBox.Show("Enter new name or replaced content  you want to do!");
+                return;
+            }
+
+            //儲存搜尋、取代的字串並歸零計數器
+            strSearch = txtSearch.Text;
+            strReplace = txtReplace.Text;
+            count = 0;
+
+            //為避免重覆點選，重新命名處理中先停用btnStart按鈕
+            btnStart.Text = "Renaming...";
+            btnStart.Enabled = false;
+
+
+            //呼叫重新命名方法
+            Hashtable ht = new Hashtable();
+            //用陣列來記錄要置換的特定字串
+            ArrayList myList = new ArrayList();
+            DoRename(txtPath.Text, ht, myList);
+        }
+
+        private void linkBlog_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://focus1921.wordpress.com/");
+        }
+
+        private void cbSearch_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbSearch.Checked == true)
+            {
+                txtSearch.Enabled = true;
+            }
+            else
+            {
+                txtSearch.Enabled = false;
+            }
+
+        }
+
+
+    }
+}
